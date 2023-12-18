ab6968e02d03158c2587aa73fae0fce17e9076cf
Stefan <stefan@jerpa.net>
2023-12-18 07:34:26 +0100

    day18

---
+++ day18/part2/part2.go
@@ -0,0 +1,46 @@
+package part2
+
+import (
+	"github.com/jerpa/adventofcode2023/day10/shared"
+	"github.com/twpayne/go-geom"
+	"strconv"
+	"strings"
+)
+
+func Part2(data []string) int {
+	x := float64(0)
+	y := float64(0)
+	coords := []geom.Coord{{0, 0}}
+	for _, v := range data {
+		s := strings.Split(v, " ")
+		steps, _ := strconv.ParseInt(s[2][2:7], 16, 32)
+		dir := shared.Direction(s[0])
+		switch s[2][7] {
+		case '0':
+			dir = shared.Right
+		case '1':
+			dir = shared.Down
+		case '2':
+			dir = shared.Left
+		case '3':
+			dir = shared.Up
+		}
+
+		for i := 0; i < int(steps); i++ {
+			switch dir {
+			case shared.Up:
+				y--
+			case shared.Down:
+				y++
+			case shared.Left:
+				x--
+			case shared.Right:
+				x++
+			}
+		}
+		coords = append(coords, geom.Coord{x, y})
+	}
+	p := geom.NewPolygon(geom.XY).MustSetCoords([][]geom.Coord{coords})
+	// https://en.wikipedia.org/wiki/Pick%27s_theorem
+	return int(p.Area() - (p.Length() / 2) + 1 + p.Length())
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/jerpa/adventofcode2023
 
 go 1.21.1
 
-require github.com/RyanCarrier/dijkstra v1.3.0 // indirect
+require (
+	github.com/RyanCarrier/dijkstra v1.3.0
+	github.com/twpayne/go-geom v1.5.3
+)
